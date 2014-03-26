5f008223ac246a0c5d4dd7d63ac88cb9a642a6aa
Jacob Emerick <jpemeric@gmail.com>
2014-03-25 19:13:41 -0500

    building out more test cases

--- examples.php
+++ examples.php
@@ -2,4 +2,6 @@
 
 include_once __DIR__ . '/vendor/autoload.php';
 
-$client = new Bigstock\OAuth2API\Client();
+$client = new Bigstock\OAuth2API\Client(false);
+$client->setClientCredentials(CLIENT_ID, CLIENT_SECRET);
+$response = $client->request('search', array('q' => 'upper peninsula'));
